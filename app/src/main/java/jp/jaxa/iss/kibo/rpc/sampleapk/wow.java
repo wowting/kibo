83c31956b603b92a9cf4c83fdfcf8f326d4a78a8
wwwhaz <a12210521@gmail.com>
2025-06-05 22:38:56 +0800

    0605

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/wow.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/wow.java
@@ -23,21 +23,21 @@ public class wow extends KiboRpcService {
         put(1, new Point(11.3, -10.007, 5.287));
       //  put(1, new Point(11.3, -10.007, 5.287));
         put(2, new Point(10.727, -8.80, 4.523));//X:11
-        put(3, new Point(10.95, -7.4, 4.523));//3.8   -7.623
-        put(4, new Point(10.50, -6.90, 4.8));//4.95
+        put(3, new Point(10.95, -7.70, 4.523));//3.8   -7.623
+        put(4, new Point(10.50, -6.573, 4.8));//4.95
     }};
 
     private static final Map<Integer, Quaternion> AREA_QUATS = new HashMap<Integer, Quaternion>() {{
         put(1, new Quaternion(0f, 0f, -0.707f, 0.707f));
         put(2, new Quaternion(0f, 0.707f, 0f, 1f));
         put(3, new Quaternion(0f, 0.707f, 0f, 1f));
-        put(4, new Quaternion(-0.707f, 0.707f, 0f, 0.707f));//0.707 0 0 0   0.707 0 0 0.707
+        put(4, new Quaternion(0f, 0.707f, 0.707f, 0f));//0.707 0 0 0   0.707 0 0 0.707 0.707 0.707
     }};
 
     private static final Map<Integer, Point> AREA_POINTS2 = new HashMap<Integer, Point>() {{
-        put(1, new Point(11.3, -9.353, 5.287));
+        put(1, new Point(11.3, -8.8, 5.287));
         //  put(1, new Point(11.3, -10.007, 5.287));
-        put(2, new Point(10.727, -8.80, 4.523));//X:11
+        put(2, new Point(11.3, -8.80, 4.523));//X:11
         put(3, new Point(10.95, -7.4, 4.523));//3.8   -7.623
         put(4, new Point(10.50, -6.90, 4.8));//4.95
     }};
@@ -46,7 +46,7 @@ public class wow extends KiboRpcService {
         put(1, new Quaternion(0f, 0f, -0.707f, 0.707f));
         put(2, new Quaternion(0f, 0.707f, 0f, 1f));
         put(3, new Quaternion(0f, 0.707f, 0f, 1f));
-        put(4, new Quaternion(-0.707f, 0.707f, 0f, 0.707f));//0.707 0 0 0   0.707 0 0 0.707
+        put(4, new Quaternion(-0.707f, 0.707f, 0f, 0f));//0.707 0 0 0   0.707 0 0 0.707
     }};
 
     private static final Point ASTRONAUT_POINT = new Point(11.143, -6.7607, 4.9654);
@@ -64,7 +64,7 @@ public class wow extends KiboRpcService {
         visitAndRecognize(1);
         try { Thread.sleep(1000); } catch (InterruptedException ignored) {}
         api.moveTo(AREA_POINTS2.get(1), AREA_QUATS2.get(1), false);
-        try { Thread.sleep(1000); } catch (InterruptedException ignored) {}
+        try { Thread.sleep(4000); } catch (InterruptedException ignored) {}
         visitAndRecognize(2);
         try { Thread.sleep(1000); } catch (InterruptedException ignored) {}
         visitAndRecognize(3);
